b638f82c76eb07d9b3dadfe11f426638e80aa965
leftsky <left.sky@vip.qq.com>
2020-10-19 17:41:35 +0800

    初始化

---
+++ src/Enter.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace leftsky\chat_api;
+
+class Enter
+{
+
+}
---
+++ src/Models/Test.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace leftsky\chat_api\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Test extends Model
+{
+    use SoftDeletes;
+}
